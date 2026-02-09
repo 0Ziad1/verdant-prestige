f503905c2c6098ddbff236669a3f3238c9931983
ziad <ziadmogadam@gmail.com>
2026-02-09 16:03:17 +0200

    whats app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
   const socialLinks = [
     { icon: Instagram, href: 'https://www.instagram.com/jovira_2026/', label: 'Instagram' },
-    { icon: MessageCircle, href: 'https://wa.me/0201128364839', label: 'WhatsApp' },
+    { icon: MessageCircle, href: 'https://wa.me/021128364839', label: 'WhatsApp' },
     { icon: Linkedin, href: 'https://www.linkedin.com/company/jovira-company', label: 'LinkedIn' },
     { icon: Facebook, href: 'https://www.facebook.com/share/1BwWGAGBvU/?mibextid=wwXIfr', label: 'Facebook' },
   ];
